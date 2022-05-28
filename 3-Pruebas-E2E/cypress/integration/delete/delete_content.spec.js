f77af71724bf71ea03dd7317e2b449ed458a4c75
Brandon <b.gonzalezp@uniandes.edu.co>
2022-05-28 11:47:00 -0500

    Eliminar contenido

--- 3-Pruebas-E2E/cypress/integration/delete/delete_content.spec.js
+++ 3-Pruebas-E2E/cypress/integration/delete/delete_content.spec.js
@@ -20,8 +20,3 @@ describe("Delete all conent", function () {
   });
 
   
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
